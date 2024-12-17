764b6ba99a723ef133221e92b10784e0cede00c8
= <=>
2024-12-17 17:29:26 +0100

    Exercici 3 fet, falta que sigui case insensitive

--- src/components/Exercici3Component.vue
+++ src/components/Exercici3Component.vue
@@ -1,5 +1,17 @@
-<script setup></script>
+<script setup>
+import users from '/data/users.json'
+import { ref, computed, watch } from 'vue'
+const userInput = ref('')
+const filteredUsers = computed(() => {
+  return users.filter(user => user.name.includes(userInput.value))
+})
+watch(userInput, (newValue, oldValue) => console.log(`Filtre canviat: ${userInput.value}`))
+</script>
 <template>
-  <h1>Exercici 3</h1>
+  <h1>Llistat d'Usuaris</h1>
+  <input type="text" v-model="userInput" placeholder="Filtra usuaris...">
+  <ul>
+    <li v-for="user in filteredUsers" :key="user.id">{{ user.name }}</li>
+  </ul>
 </template>
 <style scoped></style>
